9c7b141ad9e70d345309012bb2e6c32d08b48f9e
Mateusz Samek <samekmat@gmail.com>
2022-04-09 15:21:30 +0200

    Edit booklist template and book filter

--- Books/BooksApp/filters.py
+++ Books/BooksApp/filters.py
@@ -1,13 +1,14 @@
-from django_filters import DateFilter, CharFilter, FilterSet
+from django_filters import CharFilter, ChoiceFilter, DateFilter, FilterSet
 from .models import Book
+from django.forms import DateInput
 
 
 class BookFilter(FilterSet):
     title = CharFilter(field_name='title', lookup_expr='icontains')
     author = CharFilter(field_name='author', lookup_expr='icontains')
     publish_lang = CharFilter(field_name='publish_lang', lookup_expr='icontains')
-    start_date = DateFilter(field_name='publish_date', lookup_expr='gte')
-    end_date = DateFilter(field_name='publish_date', lookup_expr='lte')
+    start_date = DateFilter(field_name='publish_date', lookup_expr='gte', widget=DateInput(attrs={'type': 'date'}))
+    end_date = DateFilter(field_name='publish_date', lookup_expr='lte', widget=DateInput(attrs={'type': 'date'}))
 
     class Meta:
         model = Book
