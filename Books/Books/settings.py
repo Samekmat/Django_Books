bce470d4a2c427bf6c96731bafc7afbae444571b
Mateusz Samek <samekmat@gmail.com>
2022-04-09 13:08:58 +0200

    Add book model

--- Books/Books/settings.py
+++ Books/Books/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BooksApp',
+    'isbn_field',
 ]
 
 MIDDLEWARE = [
--- Books/BooksApp/models.py
+++ Books/BooksApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from isbn_field import ISBNField
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publish_date = models.DateField()
+    isbn = ISBNField()
+    page_num = models.IntegerField()
+    cover_link = models.URLField()
+    publish_lang = models.CharField(max_length=64)
