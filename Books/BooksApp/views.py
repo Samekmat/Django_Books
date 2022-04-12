01585cf5b076d926a8ef551c54f8b31e15050ad6
Mateusz Samek <samekmat@gmail.com>
2022-04-12 17:09:05 +0200

    Add pagination and 404 handle + debug set to false

--- Books/BooksApp/views.py
+++ Books/BooksApp/views.py
@@ -22,11 +22,16 @@ def index(request):
 class BookListView(ListView):
     model = Book
     template_name = "book_list.html"
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["filter"] = BookFilter(self.request.GET, queryset=self.get_queryset())
         return context
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return BookFilter(self.request.GET, queryset=queryset).qs
 
 
 class BookCreateView(CreateView):
